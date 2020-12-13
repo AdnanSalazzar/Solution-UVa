42a7f190e21e9bdf9ea0f9923d0ba4e79325a49e
Adnan Imran <69186678+AdnanSalazzar@users.noreply.github.com>
2020-12-13 12:12:06 +0600

    Create 11172	Relational Operator.cpp

---
+++ 11172	Relational Operator.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main()
+{
+    int T , i , a ,b ;
+
+    cin >> T ;
+
+    for(i = 1 ; i<= T ; i++)
+    {
+        cin >> a >> b ;
+
+        if(a == b )
+        {
+            cout << "=" <<endl;
+        }
+
+        else if(a > b)
+        {
+            cout << ">" <<endl;
+        }
+        else
+        {
+            cout << "<" <<endl;
+        }
+
+    }
+
+    return 0 ;
+
+}
